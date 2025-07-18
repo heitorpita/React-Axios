86457f48c8c115544e36724d8a6396a03a228015
heitor <c@Heitor.local>
2025-07-17 23:33:04 -0300

    feat: projeto finalizado

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,41 +1,40 @@
-import axios from 'axios'
-import React from 'react'
-import { useState, useEffect } from 'react'
-
+import blogFetch from '../axios/config'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-
 import "./Home.css"
 
-const Home = () => {
-  
-  const [posts, setPosts] = useState([])
-  
-  const getPosts = async() => {
-    try {
-      
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts"
-      )
-      
-      const data = response.data;
 
-      
-      
+const Home = () => {
+  const [posts, setPosts] = useState([])
+
+  const getPosts = async () => {
+    try {
+      const response = await blogFetch.get("/posts")
+      const data = response.data
       setPosts(data)
     } catch (error) {
-      console.log(error);
-      
+      console.log(error)
     }
-    
   }
 
-  useEffect (() => {
-    getPosts();
+  useEffect(() => {
+    getPosts()
   }, [])
 
   return (
-    <div>
-      home
+    <div className='home'>
+      <h1>Últimos Posts</h1>
+      {posts.length === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        posts.map((post) => (
+          <div className='post' key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/post/${post.id}`} className='btn'>Ler mais</Link>
+          </div>
+        ))
+      )}
     </div>
   )
 }
--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import blogFetch from "../axios/config";
+
+import { useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import "./NewPost.css";
+
 
 const NewPost = () => {
-  return (
-    <div>
-      new
-    </div>
-  )
-}
 
-export default NewPost
+  const navigate = useNavigate()
+
+  const [title, setTitle] = useState()
+  const [body, setBody] = useState()
+
+  const createPost = async (e) => {
+        e.preventDefault();
+        const post = {title, body, userID: 1};
+
+        await blogFetch.post("/post", {
+          body: post,
+        });
+
+        navigate("/")
+
+  };
+
+
+  return (
+    <div className="new-post">
+      <h2>Inserir Novo Post</h2>
+      <form onSubmit={(e) => createPost(e)}>
+        <div className="form-control">
+          <label htmlFor="title">Titulo</label>
+          <input
+            type="text"
+            name="title"
+            placeholder="Digite o Titulo"
+            id="title"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Conteudo</label>
+          <textarea name="body" id="body">Digite o Conteudo</textarea>
+        </div>
+        <input type="submit" value='Criar Post' className="btn"/>
+      </form>
+    </div>
+  );
+};
+
+export default NewPost;
